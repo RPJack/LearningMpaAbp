6523079aeb8ef04b140e2f3ac315215d4d553622
吴祥锋 <xyz17633727338@outlook.com>
2018-12-01 19:22:09 +0800

    领域层定义仓储并实现

---
+++ LearningMpaAbp.Core/IRespositories/IBackedTaskRespository.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Domain.Repositories;
+using LearningMpaAbp.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningMpaAbp.IRespositories
+{
+    /// <summary>
+    /// 自定义仓储Demo
+    /// </summary>
+    public interface IBackedTaskRespository:IRepository<MyTask>
+    {
+        /// <summary>
+        /// 获取某个用户分配了哪些任务
+        /// </summary>
+        List<MyTask> GetTaskByAssignedPersonId(long personId);
+    }
+}

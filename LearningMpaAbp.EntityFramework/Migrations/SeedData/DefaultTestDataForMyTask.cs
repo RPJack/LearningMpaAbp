901607192412c67297694e20d4081394a7b9c2a6
祥锋 吴 <xyz17633727338@outlook.com>
2018-12-03 11:19:33 +0800

    之前未提交

---
+++ LearningMpaAbp.EntityFramework/Migrations/SeedData/DefaultTestDataForMyTask.cs
@@ -0,0 +1,45 @@
+﻿using LearningMpaAbp.EntityFramework;
+using LearningMpaAbp.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningMpaAbp.Migrations.SeedData
+{
+    public class DefaultTestDataForMyTask
+    {
+        private readonly LearningMpaAbpDbContext context;
+        private static readonly List<MyTask> tasks;
+
+
+        public DefaultTestDataForMyTask(LearningMpaAbpDbContext context)
+        {
+            this.context = context;
+        }
+
+        static DefaultTestDataForMyTask()
+        {
+            tasks = new List<MyTask>()
+            {
+                new MyTask("ABP Demo","How to Learning ABP"),
+                new MyTask("ABP Demo2","Where to Learning ABP"),
+            };
+        }
+
+        public void Create()
+        {
+            foreach (var task in tasks)
+            {
+                if (context.myTask.FirstOrDefault(t => t.Title == task.Title) == null)
+                {
+                    context.myTask.Add(task);
+                }
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+}
